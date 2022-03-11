ee81efdabdf438d3f4e1dbddbff492853e04394b
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-03-11 12:05:53 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,233 @@
+import time
+import os
+
+Color34 = "\u001b[34m"
+Color37 = "\u001b[37m"
+
+
+# As you can see, its not very optimal 
+def ice1():
+    print("  + ")
+    print(" (  )   ")
+    print(" \  /  ")
+    print("  \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice2():
+    print("    + ")
+    print("   (  ) ")
+    print("   \  /   ")
+    print("    \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice3():
+    print("      + ")
+    print("     (  ) ")
+    print("     \  / ")
+    print("      \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice4():
+    print("         + ")
+    print("        (  ) ")
+    print("        \  / ")
+    print("         \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice5():
+    print("            + ")
+    print("           (  ) ")
+    print("           \  / ")
+    print("            \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice6():
+    print("              + ")
+    print("             (  ) ")
+    print("             \  / ")
+    print("              \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice7():
+    print("                + ")
+    print("               (  ) ")
+    print("               \  / ")
+    print("                \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice8():
+    print("                 + ")
+    print("                (  ) ")
+    print("                \  / ")
+    print("                 \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice9():
+    print("                   + ")
+    print("                  (  ) ")
+    print("                  \  / ")
+    print("                   \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice10():
+    print("                     + ")
+    print("                    (  ) ")
+    print("                    \  / ")
+    print("                     \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice11():
+    print("                        + ")
+    print("                       (  ) ")
+    print("                       \  / ")
+    print("                        \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice12():
+    print("                          + ")
+    print("                         (  ) ")
+    print("                         \  / ")
+    print("                          \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice13():
+    print("                            + ")
+    print("                           (  ) ")
+    print("                           \  / ")
+    print("                            \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice14():
+    print("                              + ")
+    print("                             (  ) ")
+    print("                             \  / ")
+    print("                              \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice15():
+    print("                                + ")
+    print("                               (  ) ")
+    print("                               \  / ")
+    print("                                \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice16():
+    print("                                  + ")
+    print("                                 (  ) ")
+    print("                                 \  / ")
+    print("                                  \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice17():
+    print("                                    + ")
+    print("                                   (  ) ")
+    print("                                   \  / ")
+    print("                                    \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice18():
+    print("                                      + ")
+    print("                                     (  ) ")
+    print("                                     \  / ")
+    print("                                      \/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+
+
+def ice19():
+    print("                                        + ")
+    print("                                       (  ) ")
+    print("                                       \  / ")
+    print("                                        \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+def ice20():
+    print("                                          + ")
+    print("                                         (  ) ")
+    print("                                         \  / ")
+    print("                                          \/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+
+os.system("clear")
+time.sleep(.1)
+ice1()
+time.sleep(.5)
+os.system("clear")
+ice2()
+time.sleep(.5)
+os.system("clear")
+ice3()
+time.sleep(.5)
+os.system("clear")
+ice4()
+time.sleep(.5)
+os.system("clear")
+ice5()
+time.sleep(.5)
+os.system("clear")
+ice6()
+time.sleep(.5)
+os.system("clear")
+ice7()
+time.sleep(.5)
+os.system("clear")
+ice8()
+time.sleep(.5)
+os.system("clear")
+ice9()
+time.sleep(.5)
+os.system("clear")
+ice10()
+time.sleep(.5)
+os.system("clear")
+ice11()
+time.sleep(.5)
+os.system("clear")
+ice12()
+time.sleep(.5)
+os.system("clear")
+ice13()
+time.sleep(.5)
+os.system("clear")
+ice14()
+time.sleep(.5)
+os.system("clear")
+ice15()
+time.sleep(.5)
+os.system("clear")
+ice16()
+time.sleep(.5)
+os.system("clear")
+ice17()
+time.sleep(.5)
+os.system("clear")
+ice18()
+time.sleep(.5)
+os.system("clear")
+ice19()
+time.sleep(.5)
+os.system("clear")
+ice20()
+time.sleep(.5)
+os.system("clear")
+print("WE ALL SCREAM FOR ICE CREAM")
+time.sleep(.5)
+os.system("clr")
