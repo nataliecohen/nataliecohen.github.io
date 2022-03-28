7309df384818c8ac2d92ae8a15e34d0ab0e82407
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-03-27 21:37:37 -0700

    Update fibonacci.py

--- TT1/fibonacci.py
+++ TT1/fibonacci.py
@@ -1,1 +1,13 @@
+def fibonacci():
+  num = int(input("Enter sequence length: "))
+  if num < 0:
+    print("Enter a number greater than 0")
+  else:
+    for n in range(num):
+      print(print_fib(n))
 
+def print_fib(i):
+  if i <= 1:  
+     return i  
+  else:  
+     return(print_fib(i-2) + print_fib(i-1))
