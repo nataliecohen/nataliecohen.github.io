e2464b9a3b430a7ea7d7ac07082960c69c1cd405
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-03-27 21:39:53 -0700

    Update math.py

--- TT2/math.py
+++ TT2/math.py
@@ -1,1 +1,19 @@
+class Factors:
+  def __init__(self):
+    self.factorail=[1]
 
+  def __call__(self, n):
+    if n > 0:
+      number = n
+      print("Factors of a Given Number {0} are:".format(number))
+      for value in range(1, number +1):
+        if number % value == 0:
+          print("{0}".format(value), end=" ")
+      print()
+    else:
+      print("Cannot calculate")
+
+factors_of = Factors().__call__
+print(factors_of(44))
+
+#test nums: 5, 8, 10, 200, 44 
