03eb2c69a7aae511a267e07279686af429a2558f
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-03-27 21:40:46 -0700

    Create factorial.py

---
+++ TT2/factorial.py
@@ -0,0 +1,28 @@
+class Factorial:
+  def __init__(self):
+        self.factorial = []
+    
+def __call__(self,n):
+        if n == 1 or n == 0:
+            self.print()
+            return 1
+        else:
+            self.factorial.append(n)
+            return n * self(n-1)
+    
+def print(self):
+        print(sparkles”*30, “\nSequence: \n”, *self.factorial)
+
+def run_factorial():
+    #first test
+    n = 5
+    facto = factorial()
+    result = facto(n)
+    print(sparkles”*30, “\nThe factorial of”, n, “is”, result)
+    #second test
+    n = 10
+    facto = factorial()
+    result = facto(n)
+    print(sparkles”*30, “\nThe factorial of”, n, “is”, result)
+
+      
