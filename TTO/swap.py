fc8a435d817c42a6019ada671ae2acf013283772
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-03-31 11:34:06 -0700

    Create swap.py

---
+++ TTO/swap.py
@@ -0,0 +1,15 @@
+x = 21
+y = 16
+print (x,y)
+def swap (x,y):
+    q = x
+# we set one of our variables to a temporary variable
+    x = y
+#swap the original two variables
+    y = q
+#set y equal to the temp
+
+    return x,y
+
+a,b= swap(16,21)
+print(a, b)
