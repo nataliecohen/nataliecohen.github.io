80c718b38f94dc3d35e68ea6f6b1b371b34c4327
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-04-04 14:21:32 -0700

    Update swap.py

--- TTO/swap.py
+++ TTO/swap.py
@@ -5,17 +5,17 @@ def driver():
  y = 10
 
 # To take inputs from the user
-x = input('Enter value of x: ')
-y = input('Enter value of y: ')
+ x = input('Enter value of x: ')
+ y = input('Enter value of y: ')
 
 # create a temporary variable and swap the values
-temp = x
-x = y
-y = temp
+ temp = x
+ x = y
+ y = temp
 
-print('The value of x after swapping: {}'.format(x))
-print('The value of y after swapping: {}'.format(y))
+ print('The value of x after swapping: {}'.format(x))
+ print('The value of y after swapping: {}'.format(y))
 
 
 if __name__ == "__main__":
-  driver()
+   driver()
